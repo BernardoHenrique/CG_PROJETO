b99780f8530ad8b3a659ba3ecf30e4cd50b4d28f
BernardoHenrique <bernardo.castico@gmail.com>
2022-06-10 20:16:46 +0100

    2 much stuff to mention

--- entrega3/G20-Tagus/codigo/js/trabalho3.js
+++ entrega3/G20-Tagus/codigo/js/trabalho3.js
@@ -1,10 +1,16 @@
 /*global THREE, requestAnimationFrame, console*/
 
+//import { VRButton } from 'CG_PROJETO/entrega3/G20-Tagus/codigo/VR_CONTENT/VR.js';
+
 var renderer, scene, material, geometry, mesh, camera;
 
-var lamp1, lamp2, lamp3, sphere;
+var lamp1, lamp2, lamp3, sphere, box;
 
 var spotLight1, spotLight2, spotLight3, directionalLight;
+
+var palanque, smallPalanque;
+
+var helper;
 
 var controller = {
     '1': {pressed: false, func: one},
@@ -32,38 +38,122 @@ var controller = {
     'x': {pressed: false, func: x},
     'C': {pressed: false, func: c},
     'c': {pressed: false, func: c},
+    'D': {pressed: false, func: d},
+    'd': {pressed: false, func: d},
 
 }
 
-function createDirectionalLight(){
-    directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-    directionalLight.position.set(45, 100, 0);
-    directionalLight.userData = {active: true};
+/*function createStereoCamera(){
+    'use strict';
 
-    scene.add( directionalLight );
+    document.body.appendChild( VRButton.createButton( renderer ) );
+
+    renderer.xr.enabled = true;
+
+    renderer.setAnimationLoop( function () {
+
+        renderer.render( scene, camera );
+    
+    } );
+}*/
+
+function createBigPalanque(){
+    'use strict';
+
+    palanque = new THREE.Object3D();
+
+    geometry = new THREE.BoxGeometry(300,10,300);
+
+    material = new THREE.MeshLambertMaterial({color: 0x896b49});
+
+    mesh = new THREE.Mesh(geometry, material); 
+    palanque.add(mesh);
+
+    palanque.position.set(0, -100, -40);
+
+    createSmallPalanque(palanque);
+
+    scene.add(palanque);
+
+}
+
+function createSmallPalanque(obj){
+    'use strict';
+
+    smallPalanque = new THREE.Object3D();
+
+    geometry = new THREE.BoxGeometry(300, 10, 290);
+
+    material = new THREE.MeshPhongMaterial({color: 0x896b49});
+
+    mesh = new THREE.Mesh(geometry, material);
+
+    smallPalanque.position.set(0, 10, -5);
+    smallPalanque.add(mesh);
+
+    /*createStepOne(smallpalanque);
+    createStepTwo(smallPalanque);
+    createStepThree(smallPalanque);*/
+
+    obj.add(smallPalanque)
+}
+
+
+function createFloor(){
+    box = new THREE.Object3D();
+
+    geometry = new THREE.BoxGeometry(400, 1, 400);
+
+    material = new THREE.MeshPhongMaterial({color: 0x3146ff});
+
+    mesh = new THREE.Mesh(geometry, material);
+
+    box.position.set(0, -120, -40);
+    box.add(mesh);
+
+    scene.add(box);
+}
+
+function createDirectionalLight(){
+    'use strict';
+
+    directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(70, 70, 70);
+    //directionalLight.target.position.set(-70, -70, -70);
+    
+    //directionalLight.visible = false;
+
+    scene.add( directionalLight);
+    
+    //helper = new THREE.CameraHelper(directionalLight.shadow.camera);
+    //scene.add(helper);
+    //scene.add(directionalLight.target);
 }
 
 function createSpotLights(){
     'use strict';
 
     spotLight1 = new THREE.SpotLight( 0xff666f );
-    spotLight1.position.set( 70, 70, 70 );
-    spotLight1.userData = {active: true};
+    spotLight1.position.set(-200, 70, 70);
 
     scene.add( spotLight1 );
+    //helper = new THREE.CameraHelper(spotLight1.shadow.camera);
+    //scene.add(helper);
 
     spotLight2 = new THREE.SpotLight( 0xf5463f );
-    spotLight2.position.set( -70, -70, -70 );
-    spotLight2.userData = {active: true};
+    spotLight2.position.set(-30, 70, -30);
     
     scene.add( spotLight2 );
+    //helper = new THREE.CameraHelper(spotLight2.shadow.camera);
+    //scene.add(helper);
 
 
     spotLight3 = new THREE.SpotLight( 0xf5683f );
-    spotLight3.position.set( 30, 30, 30 );
-    spotLight3.userData = {active: true};
+    spotLight3.position.set(130, 70, 30);
     
     scene.add( spotLight3 );
+    //helper = new THREE.CameraHelper(spotLight3.shadow.camera);
+    //scene.add(helper);
 }
 
 function createLamps(x, y, z){
@@ -75,7 +165,7 @@ function createLamps(x, y, z){
 
     geometry = new THREE.ConeGeometry(15, 30, 60, 1, false, 0, 7);
 
-    material = new THREE.MeshBasicMaterial({color: 0xff34f3});
+    material = new THREE.MeshPhongMaterial({color: 0xff34f3});
 
     mesh = new THREE.Mesh(geometry, material); 
     lamp1.add(mesh);
@@ -86,9 +176,9 @@ function createLamps(x, y, z){
     mesh = new THREE.Mesh(geometry, material); 
     lamp3.add(mesh);
 
-    lamp1.position.set(70, 70, 70);
-    lamp2.position.set(-70, -70, -70);
-    lamp3.position.set(30, 30, 30);
+    lamp1.position.set(-200, 70, 70);
+    lamp2.position.set(-30, 70, -30);
+    lamp3.position.set(130, 70, 30);
 
     createSphere(lamp1);
     createSphere(lamp2);
@@ -106,7 +196,7 @@ function createSphere(obj){
 
     geometry = new THREE.SphereGeometry(9, 64, 32);
 
-    material = new THREE.MeshBasicMaterial({color: 0x3146ff});
+    material = new THREE.MeshLambertMaterial({color: 0x3146ff});
 
     mesh = new THREE.Mesh(geometry, material);
 
@@ -121,9 +211,12 @@ function createScene() {
     
     scene = new THREE.Scene();
     
+    createFloor();
+    createBigPalanque();
     createLamps(10, 10, 10);
     createSpotLights();
     createDirectionalLight();
+
 }
 
 function createCamera() {
@@ -133,8 +226,8 @@ function createCamera() {
         1,
         1000);
     camera.position.x = 45 ;
-    camera.position.y = 45 ;
-    camera.position.z = 300;
+    camera.position.y = 165 ;
+    camera.position.z = 400;
     camera.lookAt(scene.position);
 }
 
@@ -178,8 +271,9 @@ function init() {
 }
 
 function animate() {
+    'use strict';
     
-    if (controller["3"].pressed){
+    /*if (controller["3"].pressed){
         camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
         camera.position.x = tableArticulated.userData.x;
         camera.position.y = tableArticulated.userData.y + 5;
@@ -191,7 +285,7 @@ function animate() {
         }
         
         camera.lookAt(tableArticulated.position);
-    }
+    }*/
 
     render();
     
@@ -201,65 +295,106 @@ function animate() {
 
 
 function three(){
+    'use strict';
 
 }
 
 function two(){
-
+    'use strict';
+    
+    camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, -1, 200);
+    camera.position.x = 0;
+    camera.position.y = -100;
+    camera.position.z = -40;
+    camera.zoom = 4;
+    camera.updateProjectionMatrix();
+    camera.lookAt(palanque.position);
 }
 
 function one(){
     'use strict';
-    camera = new THREE.PerspectiveCamera(70,
-        window.innerWidth / window.innerHeight,
-        1,
-        1000);
-    camera.position.x = 300 ;
-    camera.position.y = 300 ;
-    camera.position.z = 300;
-    camera.lookAt(scene.position);
+    createCamera();
+}
+
+function d(){
+    'use strict';
+    console.log("aaaaaaaaaa");
+    if (directionalLight.visible == false){
+        console.log("aaaaaaaaaa");
+        directionalLight.visible = true;
+    }
+    else{
+        console.log("aaaaaaaaaa");
+        directionalLight.visible = false;
+    }
 }
 
 function q(){
+    'use strict';
 
 }
 
 function w(){
+    'use strict';
 
 }
 
 function e(){
+    'use strict';
 
 }
 
 function r(){
+    'use strict';
 
 }
 
 function t(){
+    'use strict';
 
 }
 
 function y(){
+    'use strict';
 
 }
 
 function a(){
+    'use strict';
 
 }
 
 function s(){
+    'use strict';
 
 }
 
 function z(){
-
+    'use strict';
+    if (spotLight1.visible == false){
+        spotLight1.visible = true;
+    }
+    else{
+        spotLight1.visible = false;
+    }
 }
 
 function x(){
-
+    'use strict';
+    if (spotLight2.visible == false){
+        spotLight2.visible = true;
+    }
+    else{
+        spotLight2.visible = false;
+    }
 }
 
 function c(){
-
+    'use strict';
+    if (spotLight3.visible == false){
+        spotLight3.visible = true;
+    }
+    else{
+        spotLight3.visible = false;
+    }
 }
